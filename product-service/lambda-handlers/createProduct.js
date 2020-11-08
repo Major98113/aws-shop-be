e71ea36ea14fbf14b4c69b5216893fb16814e8ba
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-11-08 16:42:59 +0300

    Adding create product service

---
+++ product-service/lambda-handlers/createProduct.js
@@ -0,0 +1,29 @@
+import ProductsService from '../services/products.service';
+
+export const createProduct = async event => {
+    try{
+        const productsServiceInstance = new ProductsService( process.env );
+        
+        await productsServiceInstance.DB.connect();
+    
+        const createdProduct = await productsServiceInstance.createProduct(
+            JSON.parse( event.body )
+        );
+        
+        return {
+          statusCode: 200,
+          headers: {
+            'Access-Control-Allow-Origin': '*'
+          },
+          body: JSON.stringify( createdProduct )
+        }
+      }
+      catch(error) {
+        console.error(JSON.stringify(error));
+        
+        return {
+          statusCode: 500,
+          body: "Something went wrong!"
+        };
+      }
+}
--- product-service/services/products.service.js
+++ product-service/services/products.service.js
@@ -4,7 +4,7 @@ export default class ProductsService {
         this.DB = new PostgresDB( env );
     }
 
-    async getProductById( id ){
+    async getProductById( id ) {
         try {
             const { rows: [ product ] } = await this.DB.query(`
                 SELECT products.id, products.title, products.description, products.price, products.logo, stocks.count 
@@ -22,7 +22,7 @@ export default class ProductsService {
         
     }
 
-    async getProductsList(){
+    async getProductsList() {
         try{
             const { rows } = await this.DB.query(
                 `SELECT products.id, products.title, products.description, products.price, products.logo, stocks.count 
@@ -35,6 +35,21 @@ export default class ProductsService {
         }
         catch( error ) {
             console.log( "Method getProductsList. Error: ", error );
+        }
+    }
+
+    async createProduct( product ) {
+        try{
+            const { title, description, price, logo, count = '' } = product;
+            const resultProductRecord = await this.DB.query(
+                `INSERT INTO products ( title, description, price, logo ) VALUES
+                 ( '${ title }', '${ description }', ${ price }, '${ logo }' )`
+            );
+
+            return resultProductRecord;
+        }
+        catch( error ) {
+            console.log( "Method createProduct. Error: ", error );
         }
     }
 }
