d557fbb0a728fefb7a0fd145beee90c1e1e73a17
Maksim_Mozhorov@epam.com <Maksim_Mozhorov@epam.com>
2020-11-14 16:00:42 +0300

    Configured s3 bucket

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,5 +1,3 @@
 import * as handlers from './lambda-handlers';
 
-export const getProductById = handlers.getProductById;
-export const getAllProducts = handlers.getAllProducts;
-export const createProduct = handlers.createProduct;
+export const importProductsFile = handlers.importProductsFile;
--- import-service/lambda-handlers/importProductsFile.js
+++ import-service/lambda-handlers/importProductsFile.js
@@ -1,14 +1,27 @@
+import AWS from 'aws-sdk';
+import { BUCKET } from '../constants/bucket-constants';
+
 export const importProductsFile = async event => {
     try{
+      const S3 = new AWS.S3({ region: 'us-east-1' });
+      const params = {
+        Bucket: BUCKET,
+        Prefix: 'uploaded/'
+      }
+      
+      const s3Response = await S3.listObjectsV2( params ).promise();
+      const data = s3Response.Contents;
+
       return {
         statusCode: 200,
         headers: {
           'Access-Control-Allow-Origin': '*'
         },
-        body: "Hi, i am importProductsFile lambda"
+        body: JSON.stringify( data )
       }
     }
     catch(error) {
+      console.error( error );
       return {
         statusCode: 500,
         body: "Something went wrong!"
